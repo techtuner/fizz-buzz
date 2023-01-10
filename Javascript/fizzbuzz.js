4294d52ae3e3247ca98807222011c6f31bccaf79
techtuner <techtuner284@gmail.com>
2023-01-10 18:46:43 +0530

    Add Fizz Buzz problem in JavaScript

---
+++ Javascript/fizzbuzz.js
@@ -0,0 +1,15 @@
+function fizzBuzz(upto) {
+  for (let num = 1; num < upto + 1; num++) {
+    if (num % 3 === 0 && num % 5 === 0) {
+      console.log("FizzBuzz");
+    } else if (num % 3 === 0) {
+      console.log("Fizz");
+    } else if (num % 5 === 0) {
+      console.log("Buzz");
+    } else {
+      console.log(num);
+    }
+  }
+}
+
+fizzBuzz(100);
